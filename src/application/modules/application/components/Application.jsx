a1e24f4943eb1ec65f9c4b57424bc6c4cc209f5f
Philip Minakov <sir.cadogan@protonmail.com>
2024-03-17 20:19:05 +0400

    Use dark theme

--- src/application/modules/application/components/Application.jsx
+++ src/application/modules/application/components/Application.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import MPCtl from '@/modules/mpctl/components/MPCtl';
 import '../styles/Application.scss';
 
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
 const Application = () => {
     return (
-        <div>
-            <MPCtl/>
-        </div>
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline/>
+            <div>
+                <MPCtl/>
+            </div>
+        </ThemeProvider>
     );
 };
 
