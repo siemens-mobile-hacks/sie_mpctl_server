96ff66014deec4a678e00ad93f2b274706827f3c
Philip Minakov <sir.cadogan@protonmail.com>
2024-03-18 11:11:07 +0400

    Add DB

--- src/sie_mpctl_server/settings_dev.py
+++ src/sie_mpctl_server/settings_dev.py
@@ -1,3 +1,11 @@
 from sie_mpctl_server.settings import * # noqa
 
 DEBUG: bool = True
+
+DATABASES: dict = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sie_mpctl',
+        'USER': 'sie_mpctl',
+    }
+}
